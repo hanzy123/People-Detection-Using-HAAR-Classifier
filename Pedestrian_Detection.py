4edac846250d0ddc9644d9c9da57a738fdeb141a
pragyan-das <55824230+pragyan-das@users.noreply.github.com>
2020-01-23 15:10:42 +0530

    Update Pedestrian_Detection.py

--- Pedestrian_Detection.py
+++ Pedestrian_Detection.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 
 #Create your own classifier
-body_classifier = cv2.CascadeClassifier('C:\\Users\\pragyan.das\\Desktop\\Machine_Learning\\Deep_Learning\\Haarcascades\\haarcascade_fullbody.xml')
+body_classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 
 #Initiate video capture for video file
 cap = cv2.VideoCapture('people_walking.mp4')
@@ -13,16 +13,21 @@ while cap.isOpened():
     #time.sleep(0.5)   #this slows down the video....higher the number  more slow it is
 
     ret,frame = cap.read()
+    
+    # Resizing the video frame
     frame = cv2.resize(frame, None , fx = 0.5 , fy = 0.5, interpolation= cv2.INTER_LINEAR)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Mention the scalefactor(ex. 1.2) and minimum neighbors(ex. 3) 
     bodies = body_classifier.detectMultiScale(gray , 1.2, 3)
 
+    #Draw rectangle when a object is identified
     for (x,y,w,h) in bodies:
         cv2.rectangle(frame, (x,y) , (x+w,y+h), (0,255,255), 2)
         cv2.imshow('Pedestrians', frame)
 
-    if cv2.waitKey(1) == 13:
+    if cv2.waitKey(1) == 13:  #13 is the Enter Key
         break
 
 cap.release()
